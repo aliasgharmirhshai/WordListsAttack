d579c93bbcf8d54091f1e395a52cc4cefd5184a2
aliasgharmirhshai <lmyrshahy3@gmail.com>
2023-02-05 12:29:02 +0330

    Reform

--- WordListsAttack.py
+++ WordListsAttack.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import sys
+import datetime
 
 RED = '\033[31m'
 GREEN = '\033[32m'
@@ -12,25 +13,46 @@ WHITE = '\033[37m'
 
 def start():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(YELLOW + f"\nStart Scanning.......\n")
+    print(YELLOW + f"\n\nStarting At {datetime.datetime.now()}\n")
 
+def help():
+        print(WHITE + """
+        WordPress Usage:
+            -wordpress-plugin[Plugin], -wordpress-content[Content], -wordpress[Random]
+        WordPress Examples:
+            python3 WordListsAttack.py -wordpress-plugin [Host]
+            python3 WordListsAttack.py -wordpress [Host]
+        Enter Word List:
+            python3 WordListsAttack.py -file [Your Word List] [Host]
+        """)
 
 def query_handler():
     global main_query
     global query_count
+    global host
     
     try:
-        if sys.argv[2] == '--plugin':
-            query_file = "wp-plugins.txt"
-        elif sys.argv[2] == '--content':
-            query_file = "wp-plugins.txt"
-        elif sys.argv[2] == "--file":
-            query_file = sys.argv[3]
+        if sys.argv[1] == '-wordpress-plugin':
+            query_file = "Wordlists\wp-plugins.txt"
+            host = sys.argv[2]
+
+        elif sys.argv[1] == '-wordpress-content':
+            query_file = "Wordlists\wp-plugins.txt"
+            host = sys.argv[2]
+            
+        elif sys.argv[1] == "-wordpress":
+            query_file = "Wordlists\wordpress-random.txt"
+            host = sys.argv[2]
+
+        elif sys.argv[1] == "-file":
+            query_file = sys.argv[2]
+            host = sys.argv[3]
+
         else:
             print(RED + "Command Not Found")
+    
     except IndexError:
-        query_file = "wordpress-random.txt"
-
+        print(RED + "Please Enter Host")
 
     with open(query_file, encoding="utf-8") as main_query:
         main_query = main_query.read().split("\n")
@@ -52,17 +74,26 @@ def send_query(main_query, host, query_count):
         result = requests.get(url, headers=headers)
 
         if result.status_code == 200:
-            print(f"{GREEN} [+]{host}/{query} Is Online")
+            print(f"{GREEN} [+] {host}/{query} Is Online")
         else:
-            print(f"{RED} [-]{host}{query} Is Offline")
+            print(f"{RED} [-] {host}{query} Is Offline")
 
 
 
 if __name__ == "__main__":
     try:
-        start()
-        query_handler()
-        host = sys.argv[1]
-        send_query(main_query, host, query_count)
+        if sys.argv[1] == "-h":
+            help()
+        else:
+            start()
+            query_handler()
+            send_query(main_query, host, query_count)
+
     except KeyboardInterrupt:
         print('By......')
+    except FileNotFoundError:
+       print(RED + f"No such file or directory")
+    except IndexError:
+        help()
+    except NameError:
+        pass
